f3cc856ff3adeb39ec9b59223492b75701b40537
Szymon Jasiński <33954943+JasinskiSz@users.noreply.github.com>
2025-10-21 16:32:27 +0200

    fix: delegate the responsibility correctly

--- src/Directory.jsx
+++ src/Directory.jsx
@@ -1,19 +1,21 @@
 import Details from "./Details"
 
 function Directory({ name, type, children }) {
-    if (type === "folder") {
-        return (
-            <>
-                <Details name={name} type={type} />
-                {children.map(
-                    (child) => <Directory name={child.name} type={child.type} children={child.children} />
-                )}
-            </>
-        )
-    }
 
     return (
-        <Details name={name} type={type} />
+        <>
+            {/* Wyświetl szczegóły pliku/folderu takie jak nazwa i typ */}
+            <Details name={name} type={type} />
+            {
+                // jeśli napotkany plik będzie typu "folder", to znaczy,
+                // że ma dzieci - wyświetli je za pomocą funkcji .map()
+                type === "folder" &&
+                // jeśli typ będzie inny, ten kod się nie wykona.
+                children.map(
+                    (child) => <Directory name={child.name} type={child.type} children={child.children} />
+                )
+            }
+        </>
     )
 }
 
--- src/FileExplorer.jsx
+++ src/FileExplorer.jsx
@@ -4,11 +4,14 @@ import Directory from "./Directory"
 function FileExplorer({ fileSystem }) {
     return (
         <>
-            <Details name={fileSystem.name} type={fileSystem.type} />
-            {fileSystem.children.map(
-                (child) => <Directory name={child.name} type={child.type} children={child.children} />
-            )}
-
+            {/* Od tego momentu rozdzielamy odpowiedzialność na inne
+            komponenty dzieci. Teraz FileExplorer już nie robi wszystkiego
+            samodzielnie. On tylko przekazuje odpowiednie wartości 
+            ze zmiennej fileSystem do komponentu Directory, który dalej
+            deleguje odpowiedzialność za wyświetlanie do komponentu 
+            Details i w zależności albo wyświetla następne foldery
+            albo same szczegóły. */}
+            <Directory name={fileSystem.name} type={fileSystem.type} children={fileSystem.children} />
         </>
     )
 }
